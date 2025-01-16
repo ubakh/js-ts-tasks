267f64dfea3450eecf5cea2e1e09fc9fcc7f7acf
Uladzislau Bakharau <bakharau.uladzislau@student.ehu.lt>
2025-01-16 18:29:07 +0200

    feat: implemented HW:hometasks-05-classes

--- src/user.js
+++ src/user.js
@@ -1,5 +1,53 @@
 class User {
-  // Your code for class User here
+  #name;
+  #age;
+  #firstName;
+  #secondName;
+
+  constructor(firstName, secondName, age) {
+    if (!this.#validInput(firstName) || !this.#validInput(secondName)) {
+      throw new Error("'should not create User instance with any empty data'");
+    }
+    this.#firstName = firstName;
+    this.#secondName = secondName;
+
+    this.#name = `${this.#firstName} ${this.#secondName}`;
+
+    if (age === undefined || typeof age !== 'number') {
+      throw new Error('should not allow to create User instance with wrong data types');
+    }
+    this.#age = age;
+  }
+
+  #validInput(input) {
+    return !(input === null || input === undefined);
+  }
+
+  introduce() {
+    return `My name is ${this.#firstName} ${this.#secondName}, I'm ${this.#age}`;
+  }
+
+  set firstName(newName) {
+    this.#firstName = newName;
+    this.#name = `${newName} ${this.#secondName}`;
+  }
+
+  set secondName(newName) {
+    this.#secondName = newName;
+    this.#name = `${this.#firstName} ${newName}`;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get age() {
+    return this.#age;
+  }
+
+  celebrateBirthday() {
+    this.#age++;
+  }
 }
 
 /**
@@ -16,7 +64,7 @@ module.exports.User = User;
  * @returns {User}
  */
 module.exports.createUser = function (firstName, secondName, age) {
-  throw new Error('Not implemented'); // remove this line and put your code here
+  return new User(firstName, secondName, age);
 };
 
 /**
@@ -25,17 +73,19 @@ module.exports.createUser = function (firstName, secondName, age) {
  * @returns {Array<User>}
  */
 module.exports.createUsers = function (data) {
-  throw new Error('Not implemented'); // remove this line and put your code here
+  return data.map(user => {
+    return new User(user.firstName, user.secondName, user.age);
+  });
 };
 
 /**
  * Find Users in Array of Users who's age equals the provided age
- * @param {Array<User>} users
+ * @param {Array<Users>} users
  * @param {number} age
- * @returns {Array<User>}
+ * @returns {Array<Users>}
  */
 module.exports.findUsersByAge = function (users, age) {
-  throw new Error('Not implemented'); // remove this line and put your code here
+  return users.filter(user => user.age === age);
 };
 
 /**
@@ -44,7 +94,11 @@ module.exports.findUsersByAge = function (users, age) {
  * @returns {function(*): *[]}
  */
 module.exports.createUsersSortFn = function (TestUtils) {
-  throw new Error('Not implemented'); // remove this line and put your code here
+  return function (users) {
+    return users.sort((a, b) => {
+      return TestUtils.comparator(a, b);
+    });
+  };
 };
 
 /**
@@ -53,5 +107,16 @@ module.exports.createUsersSortFn = function (TestUtils) {
  * @return {Array<User>}
  */
 module.exports.celebrate = function (users) {
-  throw new Error('Not implemented'); // remove this line and put your code here
+  let result = [];
+  for (let i = 0; i < users.length; i++) {
+    if (isEven(i)) {
+      users[i].celebrateBirthday();
+    }
+    result.push(users[i]);
+  }
+  return result;
 };
+
+function isEven(i) {
+  return i % 2 === 0;
+}
